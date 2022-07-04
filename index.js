4d558894ff4d049a0325ce9c5d7f9eab126dd302
vhm205 <minhhuynhvu9221@gmail.com>
2022-07-04 15:59:28 +0700

    Init Web3 Storage

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const { Web3Storage, getFilesFromPath } = require('web3.storage');
+
+const initWeb3 = () => {
+  console.log({ token: process.env.WEB3_STORAGE_TOKEN })
+
+  const client = new Web3Storage({ token: process.env.WEB3_STORAGE_TOKEN })
+  return client;
+}
+
+const putFiles = async () => {
+  try {
+    const client = initWeb3();
+    const getFiles = await getFilesFromPath('./');
+    const rootCID = await client.put(getFiles, { name: 'Files Demo' });
+    console.log({ rootCID })
+
+    const result = await client.get(rootCID);
+    console.log({ result })
+
+    if(result.ok) {
+      const files = await result.files();
+      console.log({ files })
+    }
+
+  } catch(err) {
+    console.error(err)
+  } finally {
+    process.exit(0)
+  }
+}
+
+putFiles()
+
+const retrieveFiles = async () => {
+  const client = initWeb3();
+  const result = await client.get('bafybeieq5jgpmjksswvzfn44isikwahdhccnulo2pqh7fttg5gtjqyboxe')
+  const files = await result.files();
+  console.log({ files })
+
+  for(const file of files) {
+    console.log(`${file.cid} ${file.name} ${file.size}`);
+  }
+  
+  process.exit(0)
+}
+
+// retrieveFiles();
